f2ee57933aa5b6de2ff3fbc62bf40a9827c3a7a3
RuslanMRB <ruslanqweewq123@yandex.ru>
2024-06-03 01:15:42 +0300

    update @SelectClasses({CalcTest.class})

--- src/test/java/org/example/TestSuite2.java
+++ src/test/java/org/example/TestSuite2.java
@@ -1,12 +1,16 @@
 package org.example;
 
 
+import org.junit.jupiter.api.Tag;
 import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectPackages("org.example")
+// @SelectPackages("org.example")
 @IncludeTags("paramSumm")
+@Tag("testsuite2")
+@SelectClasses({CalcTest.class})
 public class TestSuite2 {
 }
